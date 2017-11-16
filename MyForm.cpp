e91f52763e23b16f7b9c8c3c6c3fffe4bde998a0
Swathi Parthibha <swathiparthibha@gmail.com>
2017-11-16 13:15:07 -0500

    update

--- MyForm.cpp
+++ MyForm.cpp
@@ -2,12 +2,10 @@
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThread]
-int main(array<String^>^ args)
-{
+void main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Connect4::MyForm form;
 	Application::Run(%form);
-
-	return 0;
 }
+
